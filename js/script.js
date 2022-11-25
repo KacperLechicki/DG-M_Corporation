fa1212ea9ff15b8ed3d6359be87f87c728beed4b
Kacper Lechicki <klechicki@poczta.fm>
2022-11-25 11:48:37 +0100

    added gallery bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
-const footerYear = document.querySelector('.footer__year');
 const body = document.querySelector('body');
 const allNavItems = document.querySelectorAll('#mobile-link');
 
@@ -20,9 +19,3 @@ allNavItems.forEach((item) => {
 
 navBtn.addEventListener('click', handleNav);
 
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear;
-	footerYear.innerText = year;
-};
-
-handleCurrentYear();
